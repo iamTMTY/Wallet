97fdeeaef42c64ed02beeb81cccf3f0d8fbf5280
TMTY <salaudeen.t@gmail.com>
2021-09-23 16:38:48 +0100

    added react to the project

---
+++ resources/js/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <>
+            <h1 className="hello">Hello from react, this is the homepage and this text should be red</h1>
+        </>
+    );
+}
+
+export default Home;
+
